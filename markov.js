c5e760dd987113ad0bffcdaa714d150b06a32baa
Chalon Lubin <chalonlubin1@gmail.com>
2023-01-03 16:49:00 -0800

    Swap @ part 1

--- markov.js
+++ markov.js
@@ -15,7 +15,7 @@ class MarkovMachine {
   /** Get markov chain: returns Map of Markov chains.
    *
    *  For text of "The cat in the hat.", chains will be:
-   * 
+   *
    *  {
    *   "The": ["cat"],
    *   "cat": ["in"],
@@ -23,11 +23,35 @@ class MarkovMachine {
    *   "the": ["hat."],
    *   "hat.": [null],
    *  }
-   * 
+   *
    * */
 
   getChains() {
     // TODO: implement this!
+
+    // builds a MAP (Object)
+    // from this: ["Hello", "I'm", "Bob."]
+    // to this: {
+    //  "Hello": ["I'm"],
+    // }
+
+    let chain = {}
+    let words = this.words
+    for (let i = 0; i < words.length; i++) {
+      // check if word is a key in obj
+      if (words[i] in chain) {
+        // if value is an array
+        if (Array.isArray(chain[words[i]])) {
+          // if word is a key in obj check if next word is not in the value array
+          if (chain[words[i]].includes(words[i+1])){
+            continue;
+          } else {
+            // if it does not exit then push it.
+            chain[words[i]].push(words[i+1])
+          }
+        }
+      }
+    }
   }
 
 
